44a9a04a6b340d570ffec79db820bcfb131377ca
Jaakko Hietikko <jhietikk22@student.oulu.fi>
2026-07-07 14:20:28 +0300

    5.2 done

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,22 @@ const App = () => {
         })
     }, [])
 
+    useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON)
+            setUser(user)
+        }
+    }, [])
+
     const handleLogin = async event => {
         event.preventDefault()
         try {
             const user = await loginService.login({ username, password })
+
+            window.localStorage.setItem(
+                'loggedBlogappUser', JSON.stringify(user)
+            )
             setUser(user)
             setUsername('')
             setPassword('')
@@ -28,46 +40,58 @@ const App = () => {
         }
     }
 
-if (user === null) {
+    const handleLogout = async event => {
+        event.preventDefault()
+        window.localStorage.removeItem('loggedBlogappUser')
+        setUser(null)
+    }
+
+
+    if (user === null) {
+        return (
+            <div>
+                <h2>Log in to application</h2>
+                <form onSubmit={handleLogin}>
+                    <div>
+                        <label>
+                            Username
+                            <input
+                                type='text'
+                                value={username}
+                                onChange={({ target }) => setUsername(target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Password
+                            <input
+                                type='password'
+                                value={password}
+                                onChange={({ target }) => setPassword(target.value)}
+                            />
+                        </label>
+                    </div>
+                    <button type='submit'>Login</button>
+                </form>
+            </div>
+        )
+    }
+
     return (
         <div>
-            <h2>Log in to application</h2>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label>
-                        Username
-                        <input
-                            type='text'
-                            value={username}
-                            onChange={({ target }) => setUsername(target.value)}
-                        />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        Password
-                        <input
-                            type='password'
-                            value={password}
-                            onChange={({ target }) => setPassword(target.value)}
-                        />
-                    </label>
-                </div>
-                <button type='submit'>Login</button>
-            </form>
+            <h2>Blogs</h2>
+            <div>
+                <form onSubmit={handleLogout} style={{display: 'flex', alignItems: 'center'}}>
+                    <p>{user.name} logged in</p>
+                    <button type='submit'>Logout</button>
+                </form>
+            </div>
+            {blogs.map(blog =>
+                <Blog key={blog.id} blog={blog} />
+            )}
         </div>
     )
-  }
-
-  return (
-    <div>
-      <h2>Blogs</h2>
-      <p>{user.name} logged in</p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
 }
 
 export default App
