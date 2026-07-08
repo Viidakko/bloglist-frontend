ee382d267b16ee1005ca59b87b20f0c7f36bf0e9
Jaakko Hietikko <jhietikk22@student.oulu.fi>
2026-07-08 11:33:53 +0300

    5.7 done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
 const Blog = ({ blog }) => {
+    const [visible, setVisible] = useState(false)
+    
     const blogStyle = {
         paddingTop: 10,
         paddingLeft: 2,
@@ -7,10 +11,23 @@ const Blog = ({ blog }) => {
         marginBottom: 5
     }
 
+    const showWhenVisible = { display: visible ? '' : 'none' }
+
+    const toggleVisibility = () => {
+        setVisible(!visible)
+    }
+
     return (
         <div style={blogStyle}>
-            {blog.title} {blog.author}
-        </div>
+            <div>
+                {blog.title} <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+            </div>
+            <div style={showWhenVisible}>
+                <p>{blog.url}</p>
+                Likes: {blog.likes} <button>like</button> 
+                <p>{blog.author}</p>
+            </div>
+        </div> 
     )
 }
 
